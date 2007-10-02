b33c5389b4c2e68bc92d1ab767652e6ead9ebd90
Andrew Kimpton <awk@awkward.org>
2007-10-01 23:16:35 -0500

    Created by svn2p4.pl. ------------------------------------------------------------------------ r34 | awk | 2007-05-22 17:36:58 -0400 (Tue, 22 May 2007) | 3 lines Changed paths:    M /recsched/trunk/Preferences.h    M /recsched/trunk/Preferences.m    M /recsched/trunk/Preferences.nib/info.nib    M /recsched/trunk/Preferences.nib/keyedobjects.nib    M /recsched/trunk/recsched.xcodeproj/project.pbxproj
    Add basic toolbar to Preferences window with placeholder items for Zap2It, Tuners and Channels
    
    
    ------------------------------------------------------------------------
    
    [git-p4: depot-paths = "//recsched/recsched/main/": change = 62]

--- Preferences.h
+++ Preferences.h
@@ -16,11 +16,17 @@ extern NSString *kScheduleDownloadDurationPrefStr;
     IBOutlet NSTextField *mDurationTextField;
     IBOutlet DiscreteDurationSlider *mDurationSlider;
     IBOutlet id mPanel;
-    
+    NSMutableDictionary *mToolbarItems; //The dictionary that holds all our "master" copies of the NSToolbarItems
 }
 
 + (Preferences *)sharedInstance;
 + (void)setupDefaults;
+
+//Required NSToolbar delegate methods
+- (NSToolbarItem *)toolbar:(NSToolbar *)toolbar itemForItemIdentifier:(NSString *)itemIdentifier willBeInsertedIntoToolbar:(BOOL)flag;    
+- (NSArray *)toolbarDefaultItemIdentifiers:(NSToolbar*)toolbar;
+- (NSArray *)toolbarAllowedItemIdentifiers:(NSToolbar*)toolbar;
+
 
 - (void)showPanel:(id)sender;	/* Shows the panel */
 
--- Preferences.m
+++ Preferences.m
@@ -17,6 +17,10 @@ const int k24HoursTick   = 9;   // 1 day
 const int k168HoursTick = 15;   // 1 week
 const int k336HoursTick = 17;   // 2 weeks
 
+NSString *kZap2ItPreferencesToolbarIdentifier = @"Zap2It";
+NSString *kTunersPreferencesToolbarIdentifier = @"Tuners";
+NSString *kChannelsPreferencesToolbarIdentifier = @"Channels";
+
 NSString *kScheduleDownloadDurationPrefStr = @"scheduleDownloadDuration";
 
 struct discreteSliderMarks
@@ -27,6 +31,48 @@ struct discreteSliderMarks
 
 const int kNumberDurationSliderTicks = 6;
 struct discreteSliderMarks kDownloadDurationSliderMarks[] = { {0, 1.0}, {2, 3.0}, {5, 12.0}, {9, 24.0}, {15, 168.0}, {17, 336} };
+
+// All NSToolbarItems have a unique identifer associated with them, used to tell your delegate/controller what 
+// toolbar items to initialize and return at various points.  Typically, for a given identifier, you need to 
+// generate a copy of your "master" toolbar item, and return it autoreleased.  The function below takes an
+// NSMutableDictionary to hold your master NSToolbarItems and a bunch of NSToolbarItem paramenters,
+// and it creates a new NSToolbarItem with those parameters, adding it to the dictionary.  Then the dictionary
+// can be used from -toolbar:itemForItemIdentifier:willBeInsertedIntoToolbar: to generate a new copy of the 
+// requested NSToolbarItem (when the toolbar wants to redraw, for instance) by simply duplicating and returning
+// the NSToolbarItem that has the same identifier in the dictionary.  Plus, it's easy to call this function
+// repeatedly to generate lots of NSToolbarItems for your toolbar.
+// -------
+// label, palettelabel, toolTip, action, and menu can all be NULL, depending upon what you want the item to do
+static void addToolbarItem(NSMutableDictionary *theDict,NSString *identifier,NSString *label,NSString *paletteLabel,NSString *toolTip,id target,SEL settingSelector, id itemContent,SEL action, NSMenu * menu)
+{
+    NSMenuItem *mItem;
+    // here we create the NSToolbarItem and setup its attributes in line with the parameters
+    NSToolbarItem *item = [[[NSToolbarItem alloc] initWithItemIdentifier:identifier] autorelease];
+    [item setLabel:label];
+    [item setPaletteLabel:paletteLabel];
+    [item setToolTip:toolTip];
+    [item setTarget:target];
+    // the settingSelector parameter can either be @selector(setView:) or @selector(setImage:).  Pass in the right
+    // one depending upon whether your NSToolbarItem will have a custom view or an image, respectively
+    // (in the itemContent parameter).  Then this next line will do the right thing automatically.
+    [item performSelector:settingSelector withObject:itemContent];
+    [item setAction:action];
+    // If this NSToolbarItem is supposed to have a menu "form representation" associated with it (for text-only mode),
+    // we set it up here.  Actually, you have to hand an NSMenuItem (not a complete NSMenu) to the toolbar item,
+    // so we create a dummy NSMenuItem that has our real menu as a submenu.
+    if (menu!=NULL)
+    {
+	// we actually need an NSMenuItem here, so we construct one
+	mItem=[[[NSMenuItem alloc] init] autorelease];
+	[mItem setSubmenu: menu];
+	[mItem setTitle: [menu title]];
+	[item setMenuFormRepresentation:mItem];
+    }
+    // Now that we've setup all the settings for this new toolbar item, we add it to the dictionary.
+    // The dictionary retains the toolbar item for us, which is why we could autorelease it when we created
+    // it (above).
+    [theDict setObject:item forKey:identifier];
+}
 
 
 @implementation Preferences
@@ -80,6 +126,42 @@ static Preferences *sSharedInstance = nil;
 {
     if (self != sSharedInstance)
       [super dealloc];	// Don't free the shared instance
+}
+
+// When we launch, we have to get our NSToolbar set up.  This involves creating a new one, adding the NSToolbarItems,
+// and installing the toolbar in our window.
+-(void)awakeFromNib
+{
+//    NSFont *theFont;
+    NSToolbar *toolbar=[[[NSToolbar alloc] initWithIdentifier:@"myToolbar"] autorelease];
+    
+    // Here we create the dictionary to hold all of our "master" NSToolbarItems.
+    mToolbarItems=[[NSMutableDictionary dictionary] retain];
+
+    // often using an image will be your standard case.  You'll notice that a selector is passed
+    // for the action (blueText:), which will be called when the image-containing toolbar item is clicked.
+    addToolbarItem(mToolbarItems,kZap2ItPreferencesToolbarIdentifier,@"Zap2It",@"Zap2It",@"Zap2It User ID & Schedule",self,@selector(setImage:), nil /* image */,@selector(showZap2ItPrefs:),NULL);
+    addToolbarItem(mToolbarItems,kTunersPreferencesToolbarIdentifier,@"Tuner",@"Tuner",@"Tuner Selection",self,@selector(setImage:), nil /* image */,@selector(showTunerPrefs:),NULL);
+    addToolbarItem(mToolbarItems,kChannelsPreferencesToolbarIdentifier,@"Channels",@"Channels",@"Customize Channels you recieve",self,@selector(setImage:), nil /* image */,@selector(showChannelPrefs:),NULL);
+     
+    // the toolbar wants to know who is going to handle processing of NSToolbarItems for it.  This controller will.
+    [toolbar setDelegate:self];
+    // If you pass NO here, you turn off the customization palette.  The palette is normally handled automatically
+    // for you by NSWindow's -runToolbarCustomizationPalette: method; you'll notice that the "Customize Toolbar"
+    // menu item is hooked up to that method in Interface Builder.  Interface Builder currently doesn't automatically 
+    // show this action (or the -toggleToolbarShown: action) for First Responder/NSWindow (this is a bug), so you 
+    // have to manually add those methods to the First Responder in Interface Builder (by hitting return on the First Responder and 
+    // adding the new actions in the usual way) if you want to wire up menus to them.
+    [toolbar setAllowsUserCustomization:YES];
+
+    // tell the toolbar that it should save any configuration changes to user defaults.  ie. mode changes, or reordering will persist. 
+    // specifically they will be written in the app domain using the toolbar identifier as the key. 
+    [toolbar setAutosavesConfiguration: YES]; 
+    
+    // tell the toolbar to show icons only by default
+    [toolbar setDisplayMode: NSToolbarDisplayModeIconOnly];
+    // install the toolbar.
+    [mPanel setToolbar:toolbar];
 }
 
 - (void)showPanel:(id)sender
@@ -161,4 +243,72 @@ static Preferences *sSharedInstance = nil;
       [self updateDurationLabel];
     }
 }
+
+- (void) showZap2ItPrefs:(id)sender
+{
+	NSLog(@"Show Zap2It Preferences");
+}
+
+- (void) showTunerPrefs:(id)sender
+{
+	NSLog(@"Show Tuner Preferences");
+}
+
+- (void) showChannelPrefs:(id)sender
+{
+	NSLog(@"Show Channel Preferences");
+}
+
+#pragma mark - Toolbar Delegates
+
+// This method is required of NSToolbar delegates.  It takes an identifier, and returns the matching NSToolbarItem.
+// It also takes a parameter telling whether this toolbar item is going into an actual toolbar, or whether it's
+// going to be displayed in a customization palette.
+- (NSToolbarItem *)toolbar:(NSToolbar *)toolbar itemForItemIdentifier:(NSString *)itemIdentifier willBeInsertedIntoToolbar:(BOOL)flag
+{
+    // We create and autorelease a new NSToolbarItem, and then go through the process of setting up its
+    // attributes from the master toolbar item matching that identifier in our dictionary of items.
+    NSToolbarItem *newItem = [[[NSToolbarItem alloc] initWithItemIdentifier:itemIdentifier] autorelease];
+    NSToolbarItem *item=[mToolbarItems objectForKey:itemIdentifier];
+    
+    [newItem setLabel:[item label]];
+    [newItem setPaletteLabel:[item paletteLabel]];
+    if ([item view]!=NULL)
+    {
+		[newItem setView:[item view]];
+    }
+    else
+    {
+		[newItem setImage:[item image]];
+    }
+    [newItem setToolTip:[item toolTip]];
+    [newItem setTarget:[item target]];
+    [newItem setAction:[item action]];
+    [newItem setMenuFormRepresentation:[item menuFormRepresentation]];
+    // If we have a custom view, we *have* to set the min/max size - otherwise, it'll default to 0,0 and the custom
+    // view won't show up at all!  This doesn't affect toolbar items with images, however.
+    if ([newItem view]!=NULL)
+    {
+		[newItem setMinSize:[[item view] bounds].size];
+		[newItem setMaxSize:[[item view] bounds].size];
+    }
+
+    return newItem;
+}
+
+// This method is required of NSToolbar delegates.  It returns an array holding identifiers for the default
+// set of toolbar items.  It can also be called by the customization palette to display the default toolbar.    
+- (NSArray *)toolbarDefaultItemIdentifiers:(NSToolbar*)toolbar
+{
+    return [NSArray arrayWithObjects:kZap2ItPreferencesToolbarIdentifier, kTunersPreferencesToolbarIdentifier, kChannelsPreferencesToolbarIdentifier,nil];
+}
+
+// This method is required of NSToolbar delegates.  It returns an array holding identifiers for all allowed
+// toolbar items in this toolbar.  Any not listed here will not be available in the customization palette.
+- (NSArray *)toolbarAllowedItemIdentifiers:(NSToolbar*)toolbar
+{
+    return [NSArray arrayWithObjects:kZap2ItPreferencesToolbarIdentifier, kTunersPreferencesToolbarIdentifier, kChannelsPreferencesToolbarIdentifier, NSToolbarSeparatorItemIdentifier, NSToolbarSpaceItemIdentifier,NSToolbarFlexibleSpaceItemIdentifier,nil];
+}
+
+
 @end
