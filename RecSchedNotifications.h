d7c8b5a1f2ba463c64a0ab16bf0e11bfb92a3f28
Andrew Kimpton <awk@awkward.org>
2007-10-01 23:48:28 -0500

    Created by svn2p4.pl. ------------------------------------------------------------------------ r67 | awk | 2007-06-07 02:42:42 -0400 (Thu, 07 Jun 2007) | 1 line Changed paths:    A /recsched/trunk/RecSchedNotifications.h    A /recsched/trunk/RecSchedNotifications.m
    Add Notification for Object Controller subclasses to update their content since new items have been added (typically after a separate thread inserts the items into a separate Managed Object Context)
    ------------------------------------------------------------------------
    
    [git-p4: depot-paths = "//recsched/recsched/main/": change = 96]

---
+++ RecSchedNotifications.h
@@ -0,0 +1,10 @@
+/*
+ *  RecSchedNotifications.h
+ *  recsched
+ *
+ *  Created by Andrew Kimpton on 6/5/07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+extern NSString *RSNotificationManagedObjectContextUpdated;
---
+++ RecSchedNotifications.m
@@ -0,0 +1,11 @@
+//
+//  RecSchedNotifications.m
+//  recsched
+//
+//  Created by Andrew Kimpton on 6/5/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "RecSchedNotifications.h"
+
+NSString *RSNotificationManagedObjectContextUpdated = @"RSNotificationManagedObjectContextUpdated";
