a758f661ee8a5ab12ee2d556892cd978b37c8763
Andrew Kimpton <awk@awkward.org>
2007-11-03 08:06:31 -0500

    Add LGPL, GPL and BSD License headers to appropriate files.
    [git-p4: depot-paths = "//recsched/recsched/main/": change = 285]

--- Z2ITSchedule.h
+++ Z2ITSchedule.h
@@ -1,10 +1,19 @@
+// libRecSchedCommon - Common code shared between UI application and background server
+// Copyright (C) 2007 Andrew Kimpton
 //
-//  Z2ITSchedule.h
-//  recsched
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-//  Created by Andrew Kimpton on 1/19/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
 //
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #import <CoreData/CoreData.h>
 
--- Z2ITStation.m
+++ Z2ITStation.m
@@ -1,13 +1,21 @@
+// libRecSchedCommon - Common code shared between UI application and background server
+// Copyright (C) 2007 Andrew Kimpton
 //
-//  Z2ITStation.m
-//  recsched
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-//  Created by Andrew Kimpton on 1/16/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
 //
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #import "Z2ITStation.h"
-#import "recsched_appDelegate.h"
 #import "Z2ITSchedule.h"
 #import "Z2ITLineupMap.h"
 #import "Z2ITLineup.h"
