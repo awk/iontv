ed961907fa08867137c1836705acea1d693664e5
Andrew Kimpton <awk@awkward.org>
2007-11-07 13:34:42 -0500

    Update to build source archive and a zip archive of the binary for use with Sparkle.
    [git-p4: depot-paths = "//recsched/recsched/main/": change = 329]

--- scripts/build.pl
+++ scripts/build.pl
@@ -5,12 +5,8 @@ $BRANCH = main;
 use Env qw(DEPOT_TOP);
 use Env qw(BRANCH);
 
-#use lib $DEPOT_TOP . "/$BRANCH/src/build_scripts/macosx";
-#use buildcmds;
 use File::Copy;
 use Date::Format;
-
-# require ($DEPOT_TOP . "/$BRANCH/src/build_scripts/version_info.pl");
 
 
 # Find the most recent Change Number
@@ -26,9 +22,17 @@ system("p4 sync //recsched/*/$BRANCH/...@$lc");
 print("Done Synchronization\n");
 
 system("xcodebuild -target 'All recsched' -configuration Release BUNDLE_VERSION=$lc");
+print("Build Complete\n");
+
+printf("Creating Package\n");
 system("/Developer/usr/bin/packagemaker --doc iOnTV.pmdoc --version $lc --out iOnTV.pkg");
 
-print("Build Complete\n");
+printf("Creating zip archive for sparkle\n");
+system("cd build/Release; zip ../../iOnTV_$lc -r9q ./iOnTV.app");
+
+printf("Creating Source Archive source-$lc\n");
+system("cd ..; tar -cf iOnTV-src-$lc.tar --exclude '*build*' ./");
+printf("Source Archive created\n");
 
 exit 0;
 
