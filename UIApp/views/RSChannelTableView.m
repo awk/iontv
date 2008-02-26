6fec24bb1113ea5a4002ef406ceaf0d887e598a6
Andrew Kimpton <awk@awkward.org>
2008-02-26 12:04:59 -0500

    Add subclass of NSTableView to select a row if the 'watch' column is hit before passing the mouse down on to the button cell. This fixes a problem with watch not watch the clicked station (since the selection is not updated until after the button is 'hit').
    [git-p4: depot-paths = "//recsched/recsched/main/": change = 407]

---
+++ UIApp/views/RSChannelTableView.m
@@ -0,0 +1,49 @@
+//  Copyright (c) 2007, Andrew Kimpton
+//  
+//  All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+//  conditions are met:
+//  
+//  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+//  in the documentation and/or other materials provided with the distribution.
+//  The names of its contributors may not be used to endorse or promote products derived from this software without specific prior
+//  written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import "RSChannelTableView.h"
+
+
+@implementation RSChannelTableView
+
+// The Channel TableView has a button in the 3rd column to launch VLC to watch the selected
+// program. However the selection is not updated until after the button action message has
+// been dispatch so we override mouseDown for this class so that clicks in the 3rd column
+// update the selection before calling into the super-class which will in turn dispatch the 
+// click to the cell.
+- (void)mouseDown:(NSEvent *)theEvent
+{
+  if ([theEvent type] == NSLeftMouseDown)
+  {
+    NSPoint localPoint = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+    if ([self columnAtPoint:localPoint] == 3)
+    {
+      [self selectRow:[self rowAtPoint:localPoint] byExtendingSelection:NO];
+    }
+  }
+  [super mouseDown:theEvent];
+}
+
+@end
