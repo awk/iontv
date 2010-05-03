ed6ce9066177cae351c70be816478f1469eee323
Andrew Kimpton <awk@awkward.org>
2010-05-03 18:01:28 -0400

    Add AtomicParsley source code

---
+++ recsched/opensource/atomicparsley/compress.cpp
@@ -0,0 +1,88 @@
+//==================================================================//
+/*
+    AtomicParsley - APar_zlib.cpp
+
+    AtomicParsley is GPL software; you can freely distribute, 
+    redistribute, modify & use under the terms of the GNU General
+    Public License; either version 2 or its successor.
+
+    AtomicParsley is distributed under the GPL "AS IS", without
+    any warranty; without the implied warranty of merchantability
+    or fitness for either an expressed or implied particular purpose.
+
+    Please see the included GNU General Public License (GPL) for 
+    your rights and further details; see the file COPYING. If you
+    cannot, write to the Free Software Foundation, 59 Temple Place
+    Suite 330, Boston, MA 02111-1307, USA.  Or www.fsf.org
+
+    Copyright ©2006-2007 puck_lock
+                                                                   */
+//==================================================================//
+
+#include "AtomicParsley.h"
+#include <zlib.h>
+
+static void* zalloc(void *opaque, unsigned int items, unsigned int size)
+{
+	return calloc(items, size);
+}
+
+static void zfree(void *opaque, void *ptr)
+{
+	free(ptr);
+}
+
+/*----------------------
+APar_zlib_inflate
+  in_buffer - pointer to already compressed data
+	in_buf_len - length of compressed data
+	out_buffer - pointer to a buffer to store decompressed/inflated data
+	out_buf_len - length of the out_buffer/max allowable decompressed size
+
+    fill
+----------------------*/
+void APar_zlib_inflate(char* in_buffer, uint32_t in_buf_len,
+	char* out_buffer, uint32_t out_buf_len)
+{
+	z_stream zlib;
+
+	memset(&zlib, 0, sizeof(zlib));
+
+	// Decompress to another buffer
+	zlib.zalloc = zalloc;
+	zlib.zfree = zfree;
+	zlib.opaque = NULL;
+	zlib.avail_out = out_buf_len +1;
+	zlib.next_out = (unsigned char*)out_buffer;
+	zlib.avail_in = in_buf_len;
+	zlib.next_in = (unsigned char*)in_buffer;
+	inflateInit(&zlib);
+	inflate(&zlib, Z_PARTIAL_FLUSH);
+	inflateEnd(&zlib);
+	return ;
+}
+
+uint32_t APar_zlib_deflate(char* in_buffer, uint32_t in_buf_len,
+	char* out_buffer, uint32_t out_buf_len)
+{
+	uint32_t compressed_bytes = 0;
+	z_stream zlib;
+
+	memset(&zlib, 0, sizeof(zlib));
+
+	// Compress(default level 6) to another buffer
+  	zlib.zalloc = zalloc;
+	zlib.zfree = zfree;
+	zlib.opaque = NULL;
+	zlib.avail_out = out_buf_len +1;
+	zlib.next_out = (unsigned char*)out_buffer;
+	zlib.avail_in = in_buf_len;
+	zlib.next_in = (unsigned char*)in_buffer;
+	zlib.total_out = 0;
+	deflateInit(&zlib, Z_DEFAULT_COMPRESSION);
+	if (Z_STREAM_END == deflate(&zlib, Z_FINISH) ) {
+		compressed_bytes = zlib.total_out;
+		deflateEnd(&zlib);
+	}
+	return compressed_bytes;
+}
---
+++ recsched/opensource/atomicparsley/config.h
@@ -0,0 +1,245 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* build binary with debug output */
+/* #undef DEBUG */
+
+/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
+   */
+#define HAVE_DIRENT_H 1
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `fseeko' function. */
+#define HAVE_FSEEKO 1
+
+/* Define to 1 if you have the `fsetpos' function. */
+#define HAVE_FSETPOS 1
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#define HAVE_GETOPT_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <linux/cdrom.h> header file. */
+/* #undef HAVE_LINUX_CDROM_H */
+
+/* Define to 1 if you have the `lroundf' function. */
+#define HAVE_LROUNDF 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <math.h> header file. */
+#define HAVE_MATH_H 1
+
+/* Define to 1 if you have the `memcmp' function. */
+#define HAVE_MEMCMP 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+/* #undef HAVE_NDIR_H */
+
+/* Define to 1 if you have the `remove' function. */
+#define HAVE_REMOVE 1
+
+/* Define to 1 if you have the `rename' function. */
+#define HAVE_RENAME 1
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/* Define to 1 if you have the `sranddev' function. */
+#define HAVE_SRANDDEV 1
+
+/* Define to 1 if you have the `sscanf' function. */
+#define HAVE_SSCANF 1
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the `strftime' function. */
+#define HAVE_STRFTIME 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strncasecmp' function. */
+#define HAVE_STRNCASECMP 1
+
+/* Define to 1 if you have the `strncmp' function. */
+#define HAVE_STRNCMP 1
+
+/* Define to 1 if you have the `strrchr' function. */
+#define HAVE_STRRCHR 1
+
+/* Define to 1 if you have the `strsep' function. */
+#define HAVE_STRSEP 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the `strtol' function. */
+#define HAVE_STRTOL 1
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/mount.h> header file. */
+#define HAVE_SYS_MOUNT_H 1
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
+/* Define to 1 if the system has the type `uintmax_t'. */
+#define HAVE_UINTMAX_T 1
+
+/* Define to 1 if the system has the type `uintptr_t'. */
+#define HAVE_UINTPTR_T 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `unsigned long long int'. */
+#define HAVE_UNSIGNED_LONG_LONG_INT 1
+
+/* Define to 1 if you have the <wchar.h> header file. */
+#define HAVE_WCHAR_H 1
+
+/* Define to 1 if you have the <windows.h> header file. */
+/* #undef HAVE_WINDOWS_H */
+
+/* Define to 1 if you have the `wmemset' function. */
+#define HAVE_WMEMSET 1
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#define HAVE_ZLIB_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://bitbucket.org/wez/atomicparsley/issues/new/"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "atomicparsley"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "atomicparsley 0.9.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "atomicparsley"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.9.3"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef was allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef was allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef was allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to the type of a signed integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int16_t */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
+
+/* Define to the widest unsigned integer type if <stdint.h> and <inttypes.h>
+   do not define. */
+/* #undef uintmax_t */
+
+/* Define to the type of an unsigned integer type wide enough to hold a
+   pointer, if such a type exists, and if the system does not define it. */
+/* #undef uintptr_t */
